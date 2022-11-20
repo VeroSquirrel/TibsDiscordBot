84e92f854a52cd3b5fdba0319f6e412316378732
Vero <dev.predationwebsite@outlook.com>
2022-11-20 16:03:31 +0100

    Small changes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Serilog;
+﻿using Discord.WebSocket;
+using Serilog;
 using Serilog.Core;
 
 namespace TibsDiscordBot
@@ -7,17 +8,39 @@ namespace TibsDiscordBot
     {
         public static Task Main(string[] args) => new Program().MainAsync();
         private readonly Logger logger;
+        private readonly DiscordSocketClient client;
+        private readonly string token;
 
         internal Program()
         {
             logger = new LoggerConfiguration()
                 .WriteTo.Console()
+                .WriteTo.File("logs/tibs_bot_logs.txt", rollingInterval: RollingInterval.Hour)
                 .CreateLogger();
+
+            client = new DiscordSocketClient();
+            client.Log += Client_Log;
+
+            try
+            {
+                token = File.ReadAllText("token.txt");
+                if (string.IsNullOrWhiteSpace(token)) { throw new Exception(); }
+            }
+            catch (Exception ex)
+            {
+                logger.Error("Token cannot be found! Fatal error, exitting...");
+            }
         }
 
         public async Task MainAsync()
         {
+            
+        }
 
+        private Task Client_Log(Discord.LogMessage msg)
+        {
+            logger.Information(msg.Message);
+            return Task.CompletedTask;
         }
     }
 }
