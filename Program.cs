4b4ce4c80d18faa4412523484d3e1e3b46de1a18
Vero <dev.predationwebsite@outlook.com>
2022-11-20 15:11:50 +0100

    Init with base files

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Serilog;
+using Serilog.Core;
+
+namespace TibsDiscordBot
+{
+    internal class Program
+    {
+        public static Task Main(string[] args) => new Program().MainAsync();
+        private readonly Logger logger;
+
+        internal Program()
+        {
+            logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+        }
+
+        public async Task MainAsync()
+        {
+
+        }
+    }
+}
